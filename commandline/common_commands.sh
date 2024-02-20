41de2e119782283ae9e2ca4c6c157dd0f65a1f00
Cory Mathis <coryrmathis@gmail.com>
2024-02-19 22:29:23 -0600

    Stuff

--- commandline/common_commands.sh
+++ commandline/common_commands.sh
@@ -24,8 +24,5 @@ alias current_branch="git rev-parse --abbrev-ref HEAD"
 #### GITHUB ####
 # Open my PRs for the current repo in webview
 alias myprs="gh pr list --author '@me' -w"
+# Open the PR based on the current branch
 alias thispr="git rev-parse --abbrev-ref HEAD | xargs gh pr view --web"
-# Open the most recent of my PRs against the current repo in webview (NOT WORKING)
-# alias mypr="gh pr list --author '@me' | grep $(git rev-parse --abbrev-ref HEAD) | awk '{print $1}' | xargs num echo num " 
-
-
